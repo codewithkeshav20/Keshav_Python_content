58fe1fe34912b822942d5ca99b4ff2a89bbefbed
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-10 22:37:54 +0530

    Add files via upload

---
+++ NOTES/extra content/car.py
@@ -0,0 +1,30 @@
+
+class car:
+    def __init__(self,name,price,year,mileage):
+        self.name=name
+        self.price=price
+        self.year=year
+        self.mileage=mileage
+
+    def drive(self):
+        print(f'u drive {self.name}')
+
+    def stop(self):
+        print(f'u stopped {self.name}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
