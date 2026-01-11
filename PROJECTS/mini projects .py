08bf93349bac92f7e315c29495fc122c7d8252a4
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-12 01:12:33 +0530

    Update mini projects .py

--- PROJECTS/mini projects .py
+++ PROJECTS/mini projects .py
@@ -435,41 +435,37 @@ for letter in cipher_text:
 print(f'orginal message:{cipher_text}')
 print(f'encrypted message:{plain_text}')"""
 
+# Alarm clock
+"""
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"alarm set for {alarm_time}")
+    sound_file = "my_music.mp3"
+    is_running=True
+
+    while is_running:
+        current_time=datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time==alarm_time:
+            print("wake up")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            is_running=False
+
+        time.sleep(1)
 
 
+if __name__=="__main__":
+    alarm_time=input("enter the alarm time (HH:MM:SS)")
+    set_alarm(alarm_time)"""
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("------------------------THE END-------------------------")
