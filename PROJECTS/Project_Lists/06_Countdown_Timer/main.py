78d666e5bbb3f890aee8fa45312f5b0c89493590
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 08:20:28 +0530

    Add countdown timer program

---
+++ PROJECTS/Project_Lists/06_Countdown_Timer/main.py
@@ -0,0 +1,12 @@
+# countdown timer program
+"""
+import time
+timer = int(input("enter the duration in seconds "))
+for x in range(timer,0,-1):
+    seconds = x%60
+    minutes = int(x/60)%60
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP! ")"""
