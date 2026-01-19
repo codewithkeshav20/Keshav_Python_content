51a41fce88a2dd45f8804ad44718f5b17bd77c4d
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 08:15:50 +0530

    Update main.py

---
+++ PROJECTS/Project_Lists/05_Compound_Interest_Calculator/main.py
@@ -0,0 +1,29 @@
+# compound interest calculator
+"""
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("enter the principle amount "))
+    if principle<0:
+        print("cant be less than 0")
+    else:
+        break
+
+while True:
+    rate = float(input("enter the rate amount "))
+    if rate<0:
+        print("cant be less than 0")
+    else:
+        break
+
+while True:
+    time = float(input("enter the time"))
+    if time<0:
+        print("cant be less than 0")
+    else:
+        break
+
+total = principle*pow((1+rate/100),time)
+print(f"balance after {time} yrs is ${total:.5f}")"""
