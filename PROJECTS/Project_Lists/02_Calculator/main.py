a9c4fe65c3425b1a82d2e35dfc5970227454d0cd
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 07:48:04 +0530

    Update main.py

---
+++ PROJECTS/Project_Lists/02_Calculator/main.py
@@ -0,0 +1,14 @@
+# calculator program
+"""
+operator = input("enter a operator (+,-,/,*)")
+num1 = float(input("enter first number"))
+num2 = float(input("enter second number"))
+
+if operator=='-':
+    result = num1-num2
+    print(round(result,2))
+elif operator=='+':
+    result = num1 + num2
+    print(round(result, 2))
+else:
+    print("wrong operator ")"""
