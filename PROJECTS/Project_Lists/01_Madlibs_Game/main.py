767a5af4a8da65db7398b7cf901ca7d1e5f95989
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 07:29:09 +0530

    Add initial implementation of Madlibs game

---
+++ PROJECTS/Project_Lists/01_Madlibs_Game/main.py
@@ -0,0 +1,10 @@
+# madlibs game
+"""
+adjective = input("enter descripption")
+adjective2 = input("enter descripption")
+noun = input("enter name,place or thing")
+verb = input("ending with 'ing' ")
+
+print(f"today i went to {adjective} place,"
+      f"where i saw {noun} also it was"
+      f"{adjective2} and i was {verb}")"""
