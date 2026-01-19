0fcd9150fcbd39c6ad27a27f3d21335ecca23559
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 08:07:37 +0530

    Add temperature conversion program

---
+++ PROJECTS/Project_Lists/04_Temperature_Converter/main.py
@@ -0,0 +1,13 @@
+# temperature conversion program
+"""
+temp = int(input("enter the temp"))
+unit = input('c,f')
+
+if unit=='c':
+    temp = 9*temp/5+32
+    print(f'temp in farenheight is {temp}')
+elif unit =='f':
+    temp =temp-32*5/9
+    print(f'temp in celcius is {temp}')
+else:
+    print("invalid")"""
