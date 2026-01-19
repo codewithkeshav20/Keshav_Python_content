f5deb707cdd71ada77440025a211b92b04b93910
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 08:03:03 +0530

    Update main.py

---
+++ PROJECTS/Project_Lists/03_Weight_Converter/main.py
@@ -0,0 +1,11 @@
+# weight conversion program
+"""weight = int(input("enter your weight "))
+unit = input("kg,lbs")
+if unit=='kg':
+    weight = weight*2.205
+    print(f"your weight in lbs is {weight}" )
+elif unit=='lbs':
+    weight = weight/2.205
+    print(f"your weight in kg is {weight}" )
+else:
+    print("enter correct unit")"""
