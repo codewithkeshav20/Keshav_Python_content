5467b1f58dd2a675a021af64a895dfc504056533
codewithkeshav20 <kishusharma2003@gmail.com>
2026-01-19 08:24:29 +0530

    Add shopping cart functionality in main.py
    Implement a shopping cart program that allows users to add food items and their prices, and calculates the total.

---
+++ PROJECTS/Project_Lists/07_Shopping_Cart/main.py
@@ -0,0 +1,25 @@
+# shopping cart program
+"""
+foods=[]
+prices=[]
+cart=[]
+total = 0
+print("welcome to shopping cart 😊")
+
+while True:
+    food = input ("enter the food u want ('q' to quit) ")
+    if food.lower()=='q':
+        break
+    else:
+        price = float(input("enter the price of food $"))
+        foods.append(food)
+        prices.append(price)
+
+print("--------------YOUR CART 🛒----------------")
+for food in foods:
+    print(food)
+for price in prices:
+    total+=price
+print("--------------YOUR TOTAL 💵----------------")
+
+print(f"{total}$")"""
